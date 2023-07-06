281de398cdd8ba9097e7f4988be5d23031422c8f
Santipro20 <santiago.lopez02@est.uexternado.edu.co>
2023-07-06 10:12:51 +0200

    New update

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.home',  # Enable the inner home (home)
-    'django_cron'
+    'apps.home'  # Enable the inner home (home)
 ]
 
 MIDDLEWARE = [
